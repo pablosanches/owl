4be88d2959c000c9dfce0de5c276345311d17742
Pablo Sanches <sanches.webmaster@gmail.com>
2017-08-11 09:27:24 -0300

    Http test

--- tests/Own/Tests/HttpTest.php
+++ tests/Own/Tests/HttpTest.php
@@ -1,13 +1,86 @@
 <?php
 namespace Own\Tests;
 
-use Own\Http;
+use Own\Method\Post;
+use Own\Method\Get;
 
 class HttpTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp(){}
+    private $endpoint = 'http://headers.jsontest.com/';
 
-    public function testInitialize()
+    public function testOptionsGet()
     {
+        $request = new Post($this->endpoint, [
+            'data' => [
+                'name' => 'foo',
+                'email' => 'foo@domain.com'
+            ]
+        ]);
+
+        $options = $request->getCurlOptions();
+        $this->assertTrue(strpos($options[10015], 'foo') !== false);
+        $this->assertTrue(strpos($options[10015], 'domain.com') !== false);
+        $this->assertEquals('POST', $options[10036]);
+
+        $request->send();
+
+        $options = $request->getCurlOptions();
+        $this->assertTrue(strpos($options[10015], 'foo') !== false);
+        $this->assertTrue(strpos($options[10015], 'domain.com') !== false);
+        $this->assertEquals('POST', $options[10036]);
+        $this->assertTrue($options[19913]);
+        $this->assertTrue($options[2]);
+    }
+
+    public function testOptionsGetWithPayload()
+    {
+        $request = new Post($this->endpoint, [
+            'data' => [
+                'name' => 'foo',
+                'email' => 'foo@domain.com'
+            ],
+            'is_payload' => true,
+            'headers' => [
+                'Authorization: foobar'
+            ]
+        ]);
+
+        $request->send();
+
+        $options = $request->getCurlOptions();
+        $this->assertEquals('POST', $options[10036]);
+        $this->assertEquals(1, $options[47]);
+        $this->assertEquals('{"name":"foo","email":"foo@domain.com"}', $options[10015]);
+        $this->assertEquals('Authorization: foobar', $options[10023][0]);
+        $this->assertEquals('Content-Type: application/json', $options[10023][1]);
+        $this->assertEquals('Content-Length: 39', $options[10023][2]);
+    }
+
+    public function testOptionsSet()
+    {
+        $request = new Get($this->endpoint);
+        $options = $request->getCurlOptions();
+
+        $this->assertNull($options[27]);
+
+        $request->setCurlOption(CURLOPT_CRLF, true);
+        $options = $request->getCurlOptions();
+
+        $this->assertTrue($options[27]);
+    }
+
+    public function testCurlInfoGet()
+    {
+        $request = new Get($this->endpoint, [
+            'autoclose' => false
+        ]);
+
+        $request->send();
+
+        $code = $request->getCurlInfo(CURLINFO_HTTP_CODE);
+
+        $request->close();
+
+        $this->assertEquals(200, $code);
     }
 }
